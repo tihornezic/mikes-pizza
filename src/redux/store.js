e9cffc5bf91de93b42da809dba08558662a53abd
Tihor Nezic <tihor.nezic@gmail.com>
2021-08-09 15:06:55 +0200

    Saving order to localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,23 @@
 import {createStore, compose, applyMiddleware} from 'redux'
+import {loadState, saveState} from './localStorage'
+import throttle from 'lodash/throttle'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './sagas/index'
 import reducers from './reducers/index.js'
 
 const sagaMiddleware = createSagaMiddleware()
 
+const persistedState = loadState()
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(sagaMiddleware)))
+// const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(sagaMiddleware)))
+const store = createStore(reducers, persistedState, composeEnhancers(applyMiddleware(sagaMiddleware)))
 
 sagaMiddleware.run(rootSaga)
+
+store.subscribe(throttle(() => {
+    saveState(store.getState())
+}, 1000))
 
 export default store
