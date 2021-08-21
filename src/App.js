117d65b97f733819b0b6dfeef1d6813e2a7d0a34
Tihor Nezic <tihor.nezic@gmail.com>
2021-08-21 11:02:48 +0200

    Home full responsive

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
           <Route exact path='/'>
             <Home />
-            {/* <Footer /> */}
+            <Footer />
           </Route>
 
           <Route path='/payment'>
--- src/components/utils/Quote.js
+++ src/components/utils/Quote.js
@@ -3,7 +3,7 @@ import pizzaLogoDarkBig from '../../img/pizzaLogoDarkBig.svg'
 const Quote = () => {
     return (
         <div className='quote'>
-            <img src={pizzaLogoDarkBig} alt='Pizza logo dark' width={'650px'} />
+            <img src={pizzaLogoDarkBig} alt='Pizza logo dark' />
             <p>"Always crispy crust & fresh ingredients. The tastiest pizza in town!"</p>
         </div>
     )
