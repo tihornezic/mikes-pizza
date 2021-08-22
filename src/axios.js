1b32721824cf8fa29ad782aefa387d56412acce8
Tihor Nezic <tihor.nezic@gmail.com>
2021-08-22 21:10:45 +0200

    baseURL to deployed google cloud url

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // the api (cloud function) url
-    baseURL: 'http://localhost:5001/mikes-pizza-c8851/us-central1/api'
+    // the api (cloud function) url (local)
+    // baseURL: 'http://localhost:5001/mikes-pizza-c8851/us-central1/api'
+    
+    // 
+    baseURL: 'https://us-central1-mikes-pizza-c8851.cloudfunctions.net/api'
 })
 
 export default instance
